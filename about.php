aba162ad19f31933e6ad4238beddd64a83603ab2
Bing Xie <xiebing.bingxie@gmail.com>
2022-11-28 15:48:03 -0500

    updated css and aboutpage Changes to be committed: 	modified:   Pictures/dandanmian.jpg 	modified:   Pictures/doufubowl.jpg 	modified:   Pictures/friedrice.jpg 	modified:   Pictures/jianjiao.jpg 	modified:   Pictures/jianjiao2.jpg 	modified:   Pictures/jianjiao3.jpg 	modified:   Pictures/mochi.jpg 	modified:   Pictures/shamianfen.jpg 	modified:   Pictures/spice.jpg 	modified:   Pictures/zongzi.jpg 	modified:   about.php 	modified:   style.css

--- about.php
+++ about.php
@@ -1,102 +1,106 @@
-<?php 
- include("subpageHeader.php");
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taste of Home</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet">
 </head>
 <body>
-
-<div class="container">
+<?php 
+    include("subpageHeader.php");
+  ?>
         <div class="aboutusbox">
             <h2 class="aboutus">ABOUT US</h2>
-            <p class="aboutusparagraph">Grew up in China and live in Canada for 
+            <p class="aboutusparagraph">We are a group of friends who are big foodies and 
+                  grew up in China. Being away from home made us going on this food making journey.
+                  Some of us went too culinary schools in China to learn the required skills and put all 
+                  our love and effort to this place. At Taste of Home, we want you to have the most authentic 
+                  experience as well as taste like what your mom would serve you at home. 
             </p>
-            <span class="aboutmeparagraph">How I describe myself:</span>
-            <ul>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Adventurous </li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Motivated </li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Hard-working</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Thoughtful</li>
-            </ul>
-            <span> How others describe me: </span>
-            <ul>
-                <li class="mylabels"><span aria-hidden="true"> · </span>Easygoing</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Courageous</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Patient</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Sincere</li>
-            </ul>
+            <span class="aboutmeparagraph">How we describe us:</span>
+            
+                 <p>Authentic </p>
+                 <p>Not your typical Chinese food </p>
+                 <p>Comfort as if you are at home</p>
+            
+         
+            <span class="aboutmeparagraph" > How others describe us: </span>
+          
+                 <p>Amazing</p>
+                 <p class="mylabels">Mind blowing delicious</p>
+                 <p class="mylabels">Homemade is the best</p>
+                 <p class="mylabels">Made with love</p>
+            
         </div>
+        
         <section class="aboutgridbox">
             <div class="aboutlist">
-                <h3>Database Systems CST2355</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="database image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Operating System Fundamentals CST8102</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="linux image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3> Object Oriented Programming (Java) CST8284</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/mochi.jpg" alt="java image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Web Programming CST8285</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/spice.jpg" alt="web image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Technical Mathematics for Computer Science MAT8001C</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/zongzi.jpg" alt="math image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Computer Essentials CST8101</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/doufubowl.jpg"
                     alt="computer essential image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Introduction to Computer Programming CST8116</h3>
+        
                 <img class="gridimage" src="../Web-Assignment02/Pictures/friedrice.jpg"
                     alt="computer programming image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Introduction to Database CST8215 </h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao2.jpg" alt="database aboutlist image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Communications ENL1813T</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/shamianfen.jpg"
+                    alt="communication aboutlist image">
+                <p></p>
+            </div>
+
+            <div class="aboutlist">
+               
+                <img class="gridimage" src="../Web-Assignment02/Pictures/baojiaozi.jpg"
                     alt="communication aboutlist image">
                 <p></p>
             </div>
         </section>
 
-
-</div>
-
-
-
-
-
-
-<?php 
+        <footer class="footer">
+  <?php 
       include("footer.php");
-?>
+    ?>
+  </footer>
 
 </body>
 </html>
