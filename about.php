6fe492e00bf3e0a0a25ebf29e285f01d9dd5d41f
Bing Xie <xiebing.bingxie@gmail.com>
2022-11-28 14:33:39 -0500

    updated about page

--- about.php
+++ about.php
@@ -1,7 +1,102 @@
 <?php 
  include("subpageHeader.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taste of Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+        <div class="aboutusbox">
+            <h2 class="aboutus">ABOUT US</h2>
+            <p class="aboutusparagraph">Grew up in China and live in Canada for 
+            </p>
+            <span class="aboutmeparagraph">How I describe myself:</span>
+            <ul>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Adventurous </li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Motivated </li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Hard-working</li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Thoughtful</li>
+            </ul>
+            <span> How others describe me: </span>
+            <ul>
+                <li class="mylabels"><span aria-hidden="true"> · </span>Easygoing</li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Courageous</li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Patient</li>
+                <li class="mylabels"> <span aria-hidden="true"> · </span>Sincere</li>
+            </ul>
+        </div>
+        <section class="aboutgridbox">
+            <div class="aboutlist">
+                <h3>Database Systems CST2355</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="database image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Operating System Fundamentals CST8102</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="linux image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3> Object Oriented Programming (Java) CST8284</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/mochi.jpg" alt="java image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Web Programming CST8285</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/spice.jpg" alt="web image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Technical Mathematics for Computer Science MAT8001C</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/zongzi.jpg" alt="math image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Computer Essentials CST8101</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/doufubowl.jpg"
+                    alt="computer essential image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Introduction to Computer Programming CST8116</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/friedrice.jpg"
+                    alt="computer programming image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Introduction to Database CST8215 </h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao2.jpg" alt="database aboutlist image">
+                <p></p>
+            </div>
+            <div class="aboutlist">
+                <h3>Communications ENL1813T</h3>
+                <img class="gridimage" src="../Web-Assignment02/Pictures/shamianfen.jpg"
+                    alt="communication aboutlist image">
+                <p></p>
+            </div>
+        </section>
+
+
+</div>
+
+
+
+
+
 
 <?php 
       include("footer.php");
 ?>
+
+</body>
+</html>
