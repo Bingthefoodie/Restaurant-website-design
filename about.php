691805fe5499e64fadfb62c6169cef75d6d949e9
Yalin <karen.s2886@gmail.com>
2022-11-29 17:03:02 -0500

    about page style updated, new css for about page

--- about.php
+++ about.php
@@ -1,26 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+  <link rel="stylesheet" href="about.css">
 </head>
+
 <body>
 <?php 
     include("subpageHeader.php");
   ?>
+  <main>
+        
         <div class="aboutusbox">
-            <h2 class="aboutus">ABOUT US</h2>
-            <p class="aboutusparagraph">We are a group of friends who are big foodies and 
+        <h2 class="aboutus">ABOUT US</h2>
+            <div class="aboutusbox-group">
+                
+            <div class="aboutboxItem">
+            <img class="aboutImage" src="../Web-Assignment02/Pictures/shamianfen.jpg" alt="communication aboutlist image">
+            </div>
+
+            <div class="aboutboxItem">
+            <p class="aboutusparagraph">
+                  We are a group of friends who are big foodies and 
                   grew up in China. Being away from home made us going on this food making journey.
                   Some of us went too culinary schools in China to learn the required skills and put all 
                   our love and effort to this place. At Taste of Home, we want you to have the most authentic 
                   experience as well as taste like what your mom would serve you at home. 
             </p>
+            </div>
+
+            <div class="aboutboxItem">
             <h3 class="aboutusparagraph">How we describe us:</h3>
             
                  <p>Authentic </p>
@@ -33,26 +46,43 @@
                  <p>Mind blowing delicious</p>
                  <p >Homemade is the best</p>
                  <p>Tasty and comforting</p>
-                 
-            
-        </div>
-        
-        <section class="aboutgridbox">
-            
-             <div class="aboutlist">
+            </div>
+            <div class="aboutboxItem">
+                <img class="aboutImage" src="/Web-Assignment02/Pictures/jianjiao2.jpg" alt="fried dumplings2">
+            </div>
+
+            <div class="aboutlist">
+                
+                <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="fried dumplings">
+            </div>
+
+            <div class="aboutboxItem">
+                <p> Dumplings! Dumplings! Dumplings! Nothing could be more versitile than dumplings. 
+                  It's eaten on every single holidays and cooked in many forms. You could have vegetarian, meat lover,
+                  cheese lover, steamed, fried, boiled, endless possibilities. All our dumplings are made by hand
+                   locally with the most fresh ingredients. 
+                </p>
+                <p><<================</p>
+            </div>  
+
+            <div class="aboutboxItem">
                 <p>When I was growing up, my mom always prepared a bowl of noodles for me every morning.
                   I recreated this dish to remind myself of that moment. 
                   This bowl of Dandan noodle, we call it DanDanMian, is full of flavours. No matter how far I am 
                   away from home. I will always remember the taste.
                   =================>>
                 </p>
-            </div>     
-        
-            <div class="aboutlist">
-                
-                <img class="gridimage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="database image">
-                <p></p>
+            </div> 
+
+            <div class="aboutboxItem">
+                <img class="aboutImage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="dandanmian">
             </div>
+
+        </div>
+        </div>
+        
+        <section class="aboutgridbox">
+            
             <div class="aboutlist">
                 
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="linux image">
@@ -112,7 +142,7 @@
                 <p></p>
             </div>
         </section>
-
+</main>
 <footer class="footer">
   <?php 
       include("footer.php");
