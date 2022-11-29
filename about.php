a64197a08c28bf2535048b25921e230366c63f03
Yalin <karen.s2886@gmail.com>
2022-11-28 20:42:54 -0500

    see update

--- about.php
+++ about.php
@@ -1,95 +1,123 @@
-<body>
-<?php 
- include("subpageHeader.php");
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taste of Home</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet">
 </head>
 <body>
-
-<div class="container">
+<?php 
+    include("subpageHeader.php");
+  ?>
         <div class="aboutusbox">
             <h2 class="aboutus">ABOUT US</h2>
-            <p class="aboutusparagraph">Grew up in China and live in Canada for 
+            <p class="aboutusparagraph">We are a group of friends who are big foodies and 
+                  grew up in China. Being away from home made us going on this food making journey.
+                  Some of us went too culinary schools in China to learn the required skills and put all 
+                  our love and effort to this place. At Taste of Home, we want you to have the most authentic 
+                  experience as well as taste like what your mom would serve you at home. 
             </p>
-            <span class="aboutmeparagraph">How I describe myself:</span>
-            <ul>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Adventurous </li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Motivated </li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Hard-working</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Thoughtful</li>
-            </ul>
-            <span> How others describe me: </span>
-            <ul>
-                <li class="mylabels"><span aria-hidden="true"> · </span>Easygoing</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Courageous</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Patient</li>
-                <li class="mylabels"> <span aria-hidden="true"> · </span>Sincere</li>
-            </ul>
+            <h3 class="aboutusparagraph">How we describe us:</h3>
+            
+                 <p>Authentic </p>
+                 <p>Not your typical Chinese food </p>
+                 <p>Made with love and care</p>
+         
+            <h3 class="aboutusparagraph" > How others describe us: </h3>
+          
+                 <p>Amazing flavours</p>
+                 <p>Mind blowing delicious</p>
+                 <p >Homemade is the best</p>
+                 <p>Tasty and comforting</p>
+                 
+            
         </div>
+        
         <section class="aboutgridbox">
+            
+             <div class="aboutlist">
+                <p>When I was growing up, my mom always prepared a bowl of noodles for me every morning.
+                  I recreated this dish to remind myself of that moment. 
+                  This bowl of Dandan noodle, we call it DanDanMian, is full of flavours. No matter how far I am 
+                  away from home. I will always remember the taste.
+                  =================>>
+                </p>
+            </div>     
+        
             <div class="aboutlist">
-                <h3>Database Systems CST2355</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="database image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Operating System Fundamentals CST8102</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="linux image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3> Object Oriented Programming (Java) CST8284</h3>
+                <p> Dumplings!Dumplings!Dumplings! Nothing could be more versitile than dumplings. 
+                  It's eaten on every single holidays and cooked in many forms. You could have vegetarian, meat lover,
+                  cheese lover, steamed, fried, boiled, endless possibilities. All our dumplings are made by hand
+                   locally with the most fresh ingredients.
+                  <<================
+                </p>
+            </div>   
+            <div class="aboutlist">
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/mochi.jpg" alt="java image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Web Programming CST8285</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/spice.jpg" alt="web image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Technical Mathematics for Computer Science MAT8001C</h3>
+                
                 <img class="gridimage" src="../Web-Assignment02/Pictures/zongzi.jpg" alt="math image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Computer Essentials CST8101</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/doufubowl.jpg"
                     alt="computer essential image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Introduction to Computer Programming CST8116</h3>
+        
                 <img class="gridimage" src="../Web-Assignment02/Pictures/friedrice.jpg"
                     alt="computer programming image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Introduction to Database CST8215 </h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao2.jpg" alt="database aboutlist image">
                 <p></p>
             </div>
             <div class="aboutlist">
-                <h3>Communications ENL1813T</h3>
+               
                 <img class="gridimage" src="../Web-Assignment02/Pictures/shamianfen.jpg"
                     alt="communication aboutlist image">
                 <p></p>
             </div>
+
+            <div class="aboutlist">
+               
+                <img class="gridimage" src="../Web-Assignment02/Pictures/baojiaozi.jpg"
+                    alt="communication aboutlist image">
+                <p></p>
+            </div>
         </section>
-</div>
-<?php 
+
+<footer class="footer">
+  <?php 
       include("footer.php");
-?>
+    ?>
+</footer>
 
 </body>
 </html>
