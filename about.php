c90d04afd15613767ff53c55b414b90367f96ccb
Bing Xie <xiebing.bingxie@gmail.com>
2022-11-28 20:34:35 -0500

    updated css on about page

--- about.php
+++ about.php
@@ -21,23 +21,33 @@
                   our love and effort to this place. At Taste of Home, we want you to have the most authentic 
                   experience as well as taste like what your mom would serve you at home. 
             </p>
-            <span class="aboutmeparagraph">How we describe us:</span>
+            <h3 class="aboutusparagraph">How we describe us:</h3>
             
                  <p>Authentic </p>
                  <p>Not your typical Chinese food </p>
-                 <p>Comfort as if you are at home</p>
-            
+                 <p>Made with love and care</p>
          
-            <span class="aboutmeparagraph" > How others describe us: </span>
+            <h3 class="aboutusparagraph" > How others describe us: </h3>
           
-                 <p>Amazing</p>
-                 <p class="mylabels">Mind blowing delicious</p>
-                 <p class="mylabels">Homemade is the best</p>
-                 <p class="mylabels">Made with love</p>
+                 <p>Amazing flavours</p>
+                 <p>Mind blowing delicious</p>
+                 <p >Homemade is the best</p>
+                 <p>Tasty and comforting</p>
+                 
             
         </div>
         
         <section class="aboutgridbox">
+            
+             <div class="aboutlist">
+                <p>When I was growing up, my mom always prepared a bowl of noodles for me every morning.
+                  I recreated this dish to remind myself of that moment. 
+                  This bowl of Dandan noodle, we call it DanDanMian, is full of flavours. No matter how far I am 
+                  away from home. I will always remember the taste.
+                  =================>>
+                </p>
+            </div>     
+        
             <div class="aboutlist">
                 
                 <img class="gridimage" src="../Web-Assignment02/Pictures/dandanmian.jpg" alt="database image">
@@ -48,6 +58,14 @@
                 <img class="gridimage" src="../Web-Assignment02/Pictures/jianjiao.jpg" alt="linux image">
                 <p></p>
             </div>
+            <div class="aboutlist">
+                <p> Dumplings!Dumplings!Dumplings! Nothing could be more versitile than dumplings. 
+                  It's eaten on every single holidays and cooked in many forms. You could have vegetarian, meat lover,
+                  cheese lover, steamed, fried, boiled, endless possibilities. All our dumplings are made by hand
+                   locally with the most fresh ingredients.
+                  <<================
+                </p>
+            </div>   
             <div class="aboutlist">
                 
                 <img class="gridimage" src="../Web-Assignment02/Pictures/mochi.jpg" alt="java image">
