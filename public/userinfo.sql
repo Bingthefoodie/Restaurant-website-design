86a14c116d1913b8eb52539238538d56b39e7d44
Bing Xie <xiebing.bingxie@gmail.com>
2022-11-30 20:56:09 -0500

    updated sql for database uses

---
+++ public/userinfo.sql
@@ -0,0 +1,31 @@
+-- Database: `demo` and php web application user
+CREATE DATABASE assignment02;
+GRANT USAGE ON *.* TO 'root';
+GRANT ALL PRIVILEGES ON assignment02.* TO 'root' ;
+FLUSH PRIVILEGES;
+
+USE assignment02;
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE IF NOT EXISTS `userinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first name` varchar(100) NOT NULL,
+  `last name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone number` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` ( `first name`,`last name`,`email`,`phone number`, `username`, `password`) VALUES
+('Roland','Berger','rberger@yahoo.ca','613-000-1234', 'RBerger', 'Rb123456'),
+('Victoria','Beckham','vbeckham@gmail.com','888-888-8888', 'VBecham','Vb123456'),
+('Jesse','Julien','julienj@gmail.com','776-124-5688', 'Jjesse','J1988j123'),
+('Larry','Schenider','Schelarry@ymail.com','909-101-6789', 'SchLarry','Slarry56789!');
