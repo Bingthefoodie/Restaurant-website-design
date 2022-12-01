8ce274779ea88cb617e42385f4d3d42c9b90beb7
YuJi-Hunt <119076943+YuJi-Hunt@users.noreply.github.com>
2022-12-01 16:28:22 -0500

    Update menu.php

--- public/menu.php
+++ public/menu.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
   <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="../Web-Assignment02/private/menuFilter.js" defer></script>
+  <script src="/Web-Assignment02/private/menuFilter.js" defer></script>
   <title>Menu</title>
   <?php 
     include("subpageHeader.php");
